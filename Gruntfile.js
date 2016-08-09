9e852ae2cb88f6f30e00630ff67b06c9c81566e5
huanghaiyang <huanghaiyang@oneapm.com>
2016-08-09 18:54:50 +0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkgFile: 'package.json',
+        clean: ['dist'],
+        babel: {
+            options: {
+                sourceMap: false
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: './lib',
+                    src: ['*.js'],
+                    dest: 'dist',
+                    ext: '.js'
+                }]
+            }
+        },
+        watch: {
+            dist: {
+                files: ['./lib/*.js'],
+                dist: ['babel:dist']
+            }
+        }
+    })
+
+    require('load-grunt-tasks')(grunt)
+    grunt.registerTask('default', ['build'])
+    grunt.registerTask('build', 'Build grunt-webdriver_stage', function () {
+        grunt.task.run([
+            'clean',
+            'babel'
+        ])
+    })
+}
---
+++ dist/index.js
@@ -0,0 +1,50 @@
+"use strict";
+
+(function () {
+	d3.sequenceChart = function (options) {
+		this.blocks = options.data || [];
+
+		this.rowPadding = options.rowPadding || 10; // default row padding px
+		this.cellPadding = options.cellPadding || 0; // default block cellpadding
+		this.blockHeight = options.blockHeight || 20; // default block height px
+		this.blockColor = options.blockColor || '#080'; // block color default blue
+
+		// TODO width and height must be a number
+
+		this.draw = function () {
+			var _this = this;
+
+			var svg = d3.select("svg");
+			var width = svg.attr("width");
+			var height = svg.attr("height");
+			var path = d3.path();
+
+			if (this.blocks.length > 0) {
+				var newBlocks = [];
+				this.blocks.forEach(function (block, index) {
+					var newBlock;
+					if (index === 0) {
+						newBlock = {
+							top: 0,
+							left: 0
+						};
+					} else {
+						var preBlock = newBlocks[index - 1];
+						newBlock = {
+							top: preBlock.top + preBlock.height + _this.rowPadding,
+							left: preBlock.left + preBlock.width + _this.cellPadding
+						};
+					}
+					newBlock.width = (block.end - block.start) * width;
+					newBlock.height = _this.blockHeight;
+					newBlocks.push(newBlock);
+				});
+			}
+			newBlocks.forEach(function (block) {
+				path.rect(block.left, block.top, block.width, block.height);
+			});
+
+			svg.append('path').attr('d', path).attr('fill', this.blockColor);
+		};
+	};
+})();
